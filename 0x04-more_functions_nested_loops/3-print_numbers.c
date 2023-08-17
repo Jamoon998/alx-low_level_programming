e2ddb266b2d4ee36f709099e2577907b90c71d7e
Jamoon <gamoon998@gmail.com>
2023-08-17 07:52:59 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - checks for checks digit (0 through 9).
+ *
+ * Return:Always 0.
+ */
+void print_number(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+}
